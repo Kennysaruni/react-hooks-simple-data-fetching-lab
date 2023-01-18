dc5d585df88c399846a93ff9a25a8a5deced52d1
Kennysaruni <kenny.saruni@student.moringaschool.com>
2023-01-18 11:47:21 +0300

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,21 @@
 // create your App component here
+import react, {useEffect}from "react"
+function App(){
+  const [dogImage, setDogImage] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+  const url = "https://dog.ceo/api/breeds/image/random";
+  useEffect(() => {
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((data) => {
+        setDogImage(data.message);
+        setLoaded(true);
+      });
+  }, []); 
+
+  if (!isLoaded) return <p>Loading...</p>;
+    return(
+        <img src={imageDog} alt="A Random Dog" />
+    )
+}
+export default App
